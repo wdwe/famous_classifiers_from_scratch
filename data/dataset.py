03bf175b3ebbd519b0cc9e88f71f18e86f554964
Xjera <DONG0089@e.ntu.edu.sg>
2020-06-13 08:18:58 -0400

    Completed EvalDataset in dataset.py

--- data/dataset.py
+++ data/dataset.py
@@ -1,7 +1,7 @@
 import torch
 import PIL
 import torchvision.transforms as tsfm
-import torchvision.datasets.ImageFolder as ImageFolder
+from torchvision.datasets import ImageFolder as ImageFolder
 
 
 class ResizeMultiple:
@@ -40,7 +40,7 @@ class FiveCropMultiple:
         crops = []
         for image in images:
             crops.extend(tsfm.FiveCrop(self.size)(image))
-        
+
         return crops
 
 class FiveAndOneCropMultiple:
@@ -50,7 +50,7 @@ class FiveAndOneCropMultiple:
     """
     def __init__(self, size):
         self.size = size
-    
+
     def __call__(self, images):
         if not isinstance(images, list):
             images = [images]
@@ -59,7 +59,7 @@ class FiveAndOneCropMultiple:
             assert image.width == image.height, "image is not square"
             crops.extend(tsfm.FiveCrop(self.size)(image))
             crops.append(tsfm.functional.resize(image, self.size))
-        
+
         return crops
 
 
@@ -83,7 +83,7 @@ class ThreeCropMultiple:
                 for step in range(3):
                     top = int(step * stride)
                     crops.append(image.crop((0, top, size, top + size)))
-            
+
         return crops
 
 
@@ -120,17 +120,29 @@ class HorizontalFlipMultiple:
             flipped.append(PIL.ImageOps.mirror(image))
         return images + flipped
 
-class ToTenorMultiple:
+class ToTensorMultiple:
     """Callable class for image transform
-    Convert a list of PIL images and stack them to a 4-D tensor
-    [num_crops, h, w, channels]
+    Convert a list of PIL images to a list of tensors
     """
     def __call__(self, images):
         if not isinstance(images, list):
             images = [images]
-        images = torch.stack([tsfm.ToTensor()(image) for image in images])
+        images = [tsfm.ToTensor()(image) for image in images]
 
         return images
+
+
+class NormalizeMultiple:
+    def __init__(self, 
+        mean = [0.485, 0.456, 0.406], 
+        std = [0.229, 0.224, 0.225]):
+        self.mean = mean
+        self.std = std
+
+    def __call__(self, tensor_images):
+        if not isinstance(tensor_images, list):
+            tensor_images = [tensor_images]
+        return [tsfm.Normalize(self.mean, self.std)(image) for image in tensor_images]
 
 
 class EvalDataset(ImageFolder):
@@ -141,8 +153,8 @@ class EvalDataset(ImageFolder):
         rescale_sizes = [256],
         center_square = False,
         crop = "fivecrop",
-        pixel_mean = [0.485, 0.456, 0.406],
-
+        mean = [0.485, 0.456, 0.406],
+        std = [0.229, 0.224, 0.225],
         horizontal_flip = True
     ):
         """Initialise the class
@@ -151,15 +163,16 @@ class EvalDataset(ImageFolder):
             imdir (str): The root directory for all images
             input_size (int): The size of network input.
                 If crop argument is None, then this argument is ignored.
-            rescale_sizes (list): The elements can be either int or tuple. 
+            rescale_sizes (list): The elements can be either int or tuple.
                 Tuple defines the exact size the image to be resized to before cropping.
                 Int defines the size the shorter image dimension to be resized to.
                 (default is False)
             center_square (bool): Whether to take the center square of the image.
-            crop (str, None): Must be of "fivecrop", "gridcrop", "inception" or None. 
+            crop (str, None): Must be of "fivecrop", "gridcrop", "inception" or None.
                 If this argument is None, the rescaled images are returned without cropping.
             horizontal_flip (bool): Whether to make copies of the crops' horizontal flip.
-
+            mean (list): The RGB pixel mean for normalization.
+            std (list): The RGB pixel standard deviation for normalization.
         Returns:
             None
         """
@@ -171,13 +184,16 @@ class EvalDataset(ImageFolder):
         self.center_square = center_square
         self.crop = crop
         self.horizontal_flip = horizontal_flip
+        self.mean = mean
+        self.std = std
         transforms = self._get_transforms()
-        self.super().__init__(root = self.imdir, transform = transforms)
-    
+        super().__init__(root = self.imdir, transform = transforms)
+
 
     def _get_transforms(self):
         transforms = []
 
+        
         transforms.append(ResizeMultiple(self.rescale_sizes))
 
         if self.center_square:
@@ -189,17 +205,34 @@ class EvalDataset(ImageFolder):
             transforms.append(GridCropMultiple(self.input_size))
         elif self.crop == "inception":
             transforms.append(ThreeCropMultiple())
-            transfroms.append(FiveAndOneCropMultiple(self.input_size))
-        
+            transforms.append(FiveAndOneCropMultiple(self.input_size))
+
         if self.horizontal_flip:
             transforms.append(HorizontalFlipMultiple())
-        
-        transforms.append(ToTensorMultiple())
 
-        transfroms = tsfm.compose(transforms)
+        transforms.append(ToTensorMultiple())
+        transforms.append(NormalizeMultiple(self.mean, self.std))
+        # convert the list of tensors to a 4-D tensor of dims
+        # [Num_crops, channels, H, W]
+        transforms.append(tsfm.Lambda(lambda crops: torch.stack(crops)))
+        transforms = tsfm.Compose(transforms)
 
         return transforms
-            
+
 
 if __name__ == "__main__":
-    pass
+    from tensorboardX import SummaryWriter
+    writer = SummaryWriter("logs/dataset/")
+    dataset_dir = "../datasets/imagenet/ILSVRC2015/Data/CLS-LOC/val"
+    mean = [0.485, 0.456, 0.406]
+    std = [0.229, 0.224, 0.225]
+    dataset = EvalDataset(
+        imdir = dataset_dir, 
+        mean = mean, 
+        std = std,
+        rescale_sizes = [256, 384, 512],
+        center_square = True,
+        crop = "gridcrop"
+        )
+    
+    print(dataset[0][0].shape)
