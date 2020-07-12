394396bac17f23ed55c06138f44e0b6551e2fe84
wdwe <DONG0089@e.ntu.edu.sg>
2020-07-12 10:14:42 -0400

    Finished training dataset with sanity check

--- data/dataset.py
+++ data/dataset.py
@@ -4,6 +4,7 @@ import torchvision
 import torchvision.transforms as tsfm
 from torchvision.datasets import ImageFolder as ImageFolder
 
+# evaluation datasets and transforms
 
 class ResizeMultiple:
     """Callable class for image transform
@@ -169,7 +170,7 @@ class EvalDataset(ImageFolder):
         mean = [0.485, 0.456, 0.406],
         std = [0.229, 0.224, 0.225],
         horizontal_flip = True,
-        interpolation = "bilinear"
+        interpolation = "bilinear",
         fname = False
     ):
         """Initialise the class
@@ -247,82 +248,196 @@ class EvalDataset(ImageFolder):
         return data
 
 
+# training dataset and transformations
 
+class TrainDataset(ImageFolder):
+    def __init__(
+        self,
+        imdir,
+        input_size = 224,
+        color_jitter = (0.4, 0.4, 0.4),
+        resize_scale = (0.08, 1.0),
+        ratio = (0.75, 1.333333333),
+        interpolation = "bilinear",
+        horizontal_flip = True,
+        mean = [0.485, 0.456, 0.406],
+        std = [0.229, 0.224, 0.225],
+        fname = False
+    ):
+        if color_jitter is not None:
+            assert len(color_jitter) in (3, 4), "color_jitter must be None or in [brightness, contrast, saturation(, hue)]"
+        self.color_jitter = color_jitter
+        self.input_size = input_size
+        self.resize_scale = resize_scale
+        self.ratio = ratio
+        if interpolation == "bilinear":
+            self.interpolation = PIL.Image.BILINEAR
+        elif interpolation == "bicubic":
+            self.interpolation = PIL.Image.BICUBIC
+        else:
+            raise Exception("interpolation must be either bilinear or bicubic")
+        self.horizontal_flip = horizontal_flip
+        self.mean = mean
+        self.std = std
+        self.fname = fname
+        transforms = self._get_transforms()
+        super().__init__(root = imdir, transform = transforms)
+
+    
+    def _get_transforms(self):
+        transforms = []
+        # add random resiced crop
+        transforms.append(
+            tsfm.RandomResizedCrop(self.input_size, scale=self.resize_scale, ratio=self.ratio, interpolation=self.interpolation)
+        )
+        # randomly flip the crop horizontally
+        if self.horizontal_flip:
+            transforms.append(tsfm.RandomHorizontalFlip(p = 0.5))
+        # apply color jittering
+        if self.color_jitter:
+            color_args = ["brightness", "contrast", "saturation", "hue"]
+            color_kwargs = dict(zip(color_args, self.color_jitter))
+            transforms.append(
+                tsfm.ColorJitter(**color_kwargs)
+            )
+        # conver the crops from PIL.Image to tensor
+        # Note that the axes are transposed from [H, W, C] to [C, H, W]
+        transforms.append(tsfm.ToTensor())
+        # normalize
+        transforms.append(tsfm.Normalize(mean = self.mean, std = self.std))
+        
+        return tsfm.Compose(transforms)
+    
+    def __getitem__(self, idx):
+        tensor, label = super().__getitem__(idx)
+        data = {"image": tensor, "label": label}
+        if self.fname:
+            data["fname"] = self.imgs[idx]
+        return data
 
 if __name__ == "__main__":
+    # # eval dataset testing
     import os
     from tensorboardX import SummaryWriter
-    writer = SummaryWriter("../logs/dataset/eval/tensorboard/")
+    # writer = SummaryWriter("../logs/dataset/eval/tensorboard/")
     imagenet_dir = "../../datasets/imagenet/ILSVRC2015/Data/CLS-LOC/"
-    val_dir = os.path.join(imagenet_dir, "val")
-    fname = True
+    # val_dir = os.path.join(imagenet_dir, "val")
+    # fname = True
     
-    # imagenet mean and std
-    mean = [0.485, 0.456, 0.406]
-    std = [0.229, 0.224, 0.225]
+    # # imagenet mean and std
+    # mean = [0.485, 0.456, 0.406]
+    # std = [0.229, 0.224, 0.225]
 
-    # create an eval config dictionary for testing
-    eval_configs = {}
-    eval_configs["AlexNet"] = {
-        "imdir" : val_dir, 
-        "mean" : mean, 
-        "std" : std,
-        "rescale_sizes" : [256],
-        "center_square" : True,
-        "crop" : "fivecrop",
-        "horizontal_flip" : True,
-        "fname" : fname
-    }
+    # # create an eval config dictionary for testing
+    # eval_configs = {}
+    # eval_configs["AlexNet"] = {
+    #     "imdir" : val_dir, 
+    #     "mean" : mean, 
+    #     "std" : std,
+    #     "rescale_sizes" : [256],
+    #     "center_square" : True,
+    #     "crop" : "fivecrop",
+    #     "horizontal_flip" : True,
+    #     "fname" : fname
+    # }
 
-    eval_configs["VGG16_dense"] = {
-        "imdir" : val_dir, 
-        "mean" : mean, 
-        "std" : std,
-        "rescale_sizes" : [256],
-        "center_square" : False,
-        "crop" : None,
-        "horizontal_flip" : False,
-        "fname" : fname
-    }
+    # eval_configs["VGG16_dense"] = {
+    #     "imdir" : val_dir, 
+    #     "mean" : mean, 
+    #     "std" : std,
+    #     "rescale_sizes" : [256],
+    #     "center_square" : False,
+    #     "crop" : None,
+    #     "horizontal_flip" : False,
+    #     "fname" : fname
+    # }
 
-    eval_configs["VGG16_Multi_Crop"] = {
-        "imdir" : val_dir, 
-        "input_size" : 224,
-        "mean" : mean, 
-        "std" : std,
-        "rescale_sizes" : [224, 256, 288],
-        "center_square" : False,
-        "crop" : "gridcrop",
-        "horizontal_flip" : True,
-        "fname" : fname
-    }
+    # eval_configs["VGG16_Multi_Crop"] = {
+    #     "imdir" : val_dir, 
+    #     "input_size" : 224,
+    #     "mean" : mean, 
+    #     "std" : std,
+    #     "rescale_sizes" : [224, 256, 288],
+    #     "center_square" : False,
+    #     "crop" : "gridcrop",
+    #     "horizontal_flip" : True,
+    #     "fname" : fname
+    # }
 
-    eval_configs["GoogLeNet"] = {
-        "imdir" : val_dir, 
-        "input_size" : 224,
-        "mean" : mean, 
-        "std" : std,
-        "rescale_sizes" : [256, 288, 320, 352],
-        "center_square" : False,
-        "crop" : "googlenet",
-        "horizontal_flip" : True,
-        "fname" : fname
-    }
+    # eval_configs["GoogLeNet"] = {
+    #     "imdir" : val_dir, 
+    #     "input_size" : 224,
+    #     "mean" : mean, 
+    #     "std" : std,
+    #     "rescale_sizes" : [256, 288, 320, 352],
+    #     "center_square" : False,
+    #     "crop" : "googlenet",
+    #     "horizontal_flip" : True,
+    #     "fname" : fname
+    # }
     
-    # Checking different eval settings
-    for eval_name in eval_configs:
-        dataset = EvalDataset(
-            **eval_configs[eval_name]
-            )
+    # # Checking different eval settings
+    # for eval_name in eval_configs:
+    #     dataset = EvalDataset(
+    #         **eval_configs[eval_name]
+    #         )
         
-        print(f"{eval_name} eval shape: ", dataset[678]['image'].shape)
-        print("Label: ", dataset[678]['label'])
-        if fname:
-            print("Filename is: ", dataset[678]['fname'])
+    #     print(f"{eval_name} eval shape: ", dataset[678]['image'].shape)
+    #     print("Label: ", dataset[678]['label'])
+    #     if fname:
+    #         print("Filename is: ", dataset[678]['fname'])
 
-        images = dataset[678]['image']
-        images = torchvision.utils.make_grid(images, normalize = True)
-        writer.add_image(eval_name, images, global_step = 0)
+    #     images = dataset[678]['image']
+    #     images = torchvision.utils.make_grid(images, normalize = True)
+    #     writer.add_image(eval_name, images, global_step = 0)
 
-    # Remember to close the writer to flush all unfinished write operation
+    # # Remember to close the writer to flush all unfinished write operation
+    # writer.close()
+
+
+    # training dataset testing
+    import random
+    import cv2
+    import numpy as np
+
+    train_dir = os.path.join(imagenet_dir, "train")
+    train_config = {
+        "imdir" : train_dir,
+        "input_size" : 224,
+        "color_jitter" : (0.4, 0.4, 0.4),
+        "resize_scale" : (0.08, 1.0),
+        "ratio" : (0.75, 1.333333333),
+        "interpolation" : "bilinear",
+        "horizontal_flip" : True,
+        "mean" : [0.485, 0.456, 0.406],
+        "std" : [0.229, 0.224, 0.225],
+        "fname" : True
+    }
+
+
+    dataset = TrainDataset(**train_config)
+    writer = SummaryWriter("../logs/dataset/train/tensorboard/")
+    
+    # randomly checking 10 training crops
+    for step in range(10):
+        idx = random.randint(0, len(dataset) - 1)
+        data = dataset[idx]
+        image = data["image"]
+        label = data["label"]
+        filename = data["fname"][0]
+        # use utils.make_grid to rescale the image back
+        # It takes a 4D tensor or a list of tensors
+        image = torchvision.utils.make_grid([image], normalize = True)
+        # load the original image
+        original_image = np.array(PIL.Image.open(filename))
+        # transpose the axes from [H, W, C] to [C, H, W]
+        original_image = np.transpose(original_image, (2, 0, 1))
+        writer.add_image("training_crop", image, global_step = step)
+        writer.add_image("original_image", original_image, global_step = step)
+    # remember to close the writer to flush all the write operation
     writer.close()
+
+
+
+
+
